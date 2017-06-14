40db9003176795d15b62aea1b15dd7121594f078
vibhu <vibhumishra808@gmail.com>
2017-06-14 10:50:51 +0530

    second commit

---
+++ SumTwoLinklist/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumTwoLinklist
+{
+    class Program
+    {
+        static LinkList firstLinkList = new LinkList();
+        static LinkList secondLinkList = new LinkList();
+        static void Main(string[] args)
+        {
+            Node node1 = new Node();
+            node1.value = 6;
+            node1.next = null;
+
+            Node node2 = new Node();
+            node2.value = 1;
+            node2.next = node1;
+
+            Node node3 = new Node();
+            node3.value = 7;
+            node3.next = node2;
+
+            firstLinkList.head = node3;
+            firstLinkList.current = node1;
+
+            Node node4 = new Node();
+            node4.value = 2;
+            node4.next = null;
+
+            Node node5 = new Node();
+            node5.value = 9;
+            node5.next = node4;
+
+            Node node6 = new Node();
+            node6.value = 5;
+            node6.next = node5;
+
+            secondLinkList.head = node6;
+            secondLinkList.current = node4;
+
+            SumLinkList();
+            Console.ReadLine();
+
+        }
+
+        public static void SumLinkList()
+        {
+            getNumber(firstLinkList.head);
+
+        }
+
+        public static void getNumber(Node next)
+        {
+            if (next == null)
+                return;
+
+        getNumber(next.next);
+        Console.Write(next.value);
+        }
+
+    }
+
+    public class Node
+    {
+        public int value;
+        public Node next;
+    }
+
+    public class LinkList
+    {
+        public Node head;
+        public Node current;
+    }
+}
